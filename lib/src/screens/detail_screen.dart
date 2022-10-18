a11ea9085ff8274297305c88b82231b5e97e7d5b
favioamarillapy <53306199+favioamarillapy@users.noreply.github.com>
2022-10-18 09:24:06 -0300

    Created movie details page

--- lib/src/screens/detail_screen.dart
+++ lib/src/screens/detail_screen.dart
@@ -1,13 +1,123 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_movies/src/widgets/widgets.dart';
 
 class DetailScreen extends StatelessWidget {
-  
+  @override
+  Widget build(BuildContext context) {
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+
+    return Scaffold(
+        body: CustomScrollView(
+      slivers: [
+        _CustomAppBar(),
+        SliverList(
+          delegate: SliverChildListDelegate(
+              [_PosterAndTitle(), _Overview(), CastingCards()]),
+        )
+      ],
+    ));
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  const _CustomAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Center(child: Text("Detail screen")),
+    return SliverAppBar(
+      backgroundColor: Colors.indigo,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black12,
+          padding: EdgeInsets.only(bottom: 10),
+          child: Text(
+            "movie.title",
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+        background: FadeInImage(
+          placeholder: AssetImage("assets/loading.gif"),
+          image: NetworkImage("https://via.placeholder.com/500x300"),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: FadeInImage(
+              placeholder: AssetImage("assets/no-image.jpg"),
+              image: NetworkImage("https://via.placeholder.com/200x300"),
+              height: 150,
+              fit: BoxFit.cover,
+            ),
+          ),
+          SizedBox(width: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "movie.title",
+                style: textTheme.headline5,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Text(
+                "movie.originalTitle",
+                style: textTheme.subtitle1,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.star_outline,
+                    size: 15,
+                    color: Colors.grey,
+                  ),
+                  SizedBox(width: 20),
+                  Text("movie.voteAverage", style: textTheme.caption),
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _Overview extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: Text(
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        textAlign: TextAlign.justify,
+        style: textTheme.subtitle1,
       ),
     );
   }
--- lib/src/widgets/widgets.dart
+++ lib/src/widgets/widgets.dart
@@ -1,2 +1,3 @@
 export 'package:flutter_movies/src/widgets/card_swiper.dart';
+export 'package:flutter_movies/src/widgets/casting_cards.dart';
 export 'package:flutter_movies/src/widgets/movie_slider.dart';
