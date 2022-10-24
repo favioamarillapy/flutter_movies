467239bcd8f503ffd8e7919c3f131c25f07d40ff
favioamarillapy <53306199+favioamarillapy@users.noreply.github.com>
2022-10-24 11:40:47 -0300

    Added hero animation

--- lib/src/screens/detail_screen.dart
+++ lib/src/screens/detail_screen.dart
@@ -71,14 +71,16 @@ class _PosterAndTitle extends StatelessWidget {
       padding: EdgeInsets.symmetric(horizontal: 20),
       child: Row(
         children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: FadeInImage(
-              placeholder: AssetImage("assets/no-image.jpg"),
-              image: NetworkImage(movie.fullPosterImg),
-              height: 150,
-              width: 110,
-              fit: BoxFit.cover,
+          Hero(
+            tag: movie.heroId!,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: FadeInImage(
+                placeholder: AssetImage("assets/no-image.jpg"),
+                image: NetworkImage(movie.fullPosterImg),
+                height: 150,
+                fit: BoxFit.cover,
+              ),
             ),
           ),
           SizedBox(width: 20),
